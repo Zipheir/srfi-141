5ccad149ba5484f60eb6528e94dc0b7a6c75f0bf
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-04-24 21:49:53 -0400

    Update module declaration.

--- srfi-141.scm
+++ srfi-141.scm
@@ -1,13 +1,14 @@
-(module srfi-141 ()
-  (import scheme)
-  (import (only (chicken base) include))
-  (import (chicken module))
-  (export ceiling/ ceiling-quotient ceiling-remainder)
-  (export floor/ floor-quotient floor-remainder)
-  (export truncate/ truncate-quotient truncate-remainder)
-  (export round/ round-quotient round-remainder)
-  (export euclidean/ euclidean-quotient euclidean-remainder)
-  (export balanced/ balanced-quotient balanced-remainder)
-  (export ceiling/ ceiling-quotient ceiling-remainder)
+(module srfi-141
+  (ceiling/ ceiling-quotient ceiling-remainder
+   floor/ floor-quotient floor-remainder
+   truncate/ truncate-quotient truncate-remainder
+   round/ round-quotient round-remainder
+   euclidean/ euclidean-quotient euclidean-remainder
+   balanced/ balanced-quotient balanced-remainder
+   ceiling/ ceiling-quotient ceiling-remainder)
+
+  (import scheme
+          (only (chicken base) include receive exact-integer?))
+
   (include "srfi-141-impl.scm")
 )
